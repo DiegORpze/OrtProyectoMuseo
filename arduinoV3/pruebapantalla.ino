04839c7ddd60bc1308946de7632f7955524e5e1a
DiegORpze <diego.pesce.uy@gmail.com>
2026-06-22 15:50:33 -0300

    codigo para probar la pantalla

---
+++ arduinoV3/pruebapantalla.ino
@@ -0,0 +1,27 @@
+#include <SPI.h>
+#include <TFT_eSPI.h>
+
+TFT_eSPI tft = TFT_eSPI();
+
+void setup() {
+  tft.init();
+  tft.setRotation(1);
+
+  tft.fillScreen(TFT_RED);
+  delay(1000);
+
+  tft.fillScreen(TFT_GREEN);
+  delay(1000);
+
+  tft.fillScreen(TFT_BLUE);
+  delay(1000);
+
+  tft.fillScreen(TFT_BLACK);
+
+  tft.setTextColor(TFT_WHITE);
+  tft.setTextSize(2);
+  tft.drawString("PANTALLA OK", 30, 110);
+}
+
+void loop() {
+}
